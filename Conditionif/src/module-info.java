17109a9ab4d78499e360ecdb98b68a01230f3a94
nguyenvananhtu <113655683+nguyenvananhtu@users.noreply.github.com>
2022-10-20 10:53:24 +0700

    Add files via upload

---
+++ Conditionif/src/module-info.java
@@ -0,0 +1,2 @@
+module Conditionif {
+}
---
+++ Operator/src/basic/dev/MainApp.java
@@ -0,0 +1,11 @@
+package basic.dev;
+
+public class MainApp {
+	public static void main(String[] args) {
+		// TODO Auto-generatic method stub
+		int a = 10;
+		a +=2; // a= a + 2;
+		System.out.println(a);
+	
+	}
+}
